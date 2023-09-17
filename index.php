1fca263052289b69808e73fc40511717d2947c81
Konstantin Protzen <code@kondev.de>
2023-09-17 13:06:23 +0200

    🚚 Local resource leafletjs

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ if ($stmt = $con->prepare("SELECT * FROM " . $settings["db"]["tables"]["markers"
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Nett-Hier-Sticker Map</title>
   <link rel="stylesheet" href="style.css" />
-  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
+  <link rel="stylesheet" href="leaflet.css" />
 </head>
 
 <body>
@@ -68,7 +68,7 @@ if ($stmt = $con->prepare("SELECT * FROM " . $settings["db"]["tables"]["markers"
       </div>
     </div>
   </main>
-  <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
+  <script src="leaflet.js"></script>
   <script src="map.js"></script>
   <script src="cookie-banner.js"></script>
   <script src="script.js"></script>
--- map.js
+++ map.js
@@ -1,12 +1,5 @@
-
 // Create map Centered on Stuttgart
 var map = L.map("map").setView([48.778026, 9.179764], 8);
-
-// Add OSM layer to map
-L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-}).addTo(map);
 
 var netthier_laend = L.icon({
     iconUrl: '/images/markers/netthier_laend.png',
@@ -26,5 +19,12 @@ var thelaend_round = L.icon({
     iconUrl: '/images/markers/thelaend_round.png',
     iconSize: [50, 50],
     iconAnchor: [25, 25],
+    popupAnchor: [-3, -76]
+});
+
+var notbad_laend = L.icon({
+    iconUrl: '/images/markers/notbad_laend.png',
+    iconSize: [66, 38],
+    iconAnchor: [33, 19],
     popupAnchor: [-3, -76]
 });
